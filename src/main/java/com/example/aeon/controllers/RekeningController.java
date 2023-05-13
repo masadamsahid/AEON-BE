d62db1ba77ff9bd6cf885d41c3fbd38f0e3477f3
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-05-14 06:08:53 +0700

    add `RekeningController`

---
+++ src/main/java/com/example/aeon/controllers/RekeningController.java
@@ -0,0 +1,12 @@
+package com.example.aeon.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rekening")
+public class RekeningController {
+
+
+
+}
