6e92f6c83a6952b27edee93df6c652adb8614c09
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-05-14 01:50:48 +0700

    add `TrainingRepository`

---
+++ src/main/java/com/example/aeon/repositories/TrainingRepository.java
@@ -0,0 +1,9 @@
+package com.example.aeon.repositories;
+
+import com.example.aeon.entities.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainingRepository extends JpaRepository<Training, Long> {
+}
