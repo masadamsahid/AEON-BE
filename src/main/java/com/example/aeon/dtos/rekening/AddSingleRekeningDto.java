ef43b4a9dc31aa76156ac0ee0f07f50f663c55d7
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-05-14 06:56:46 +0700

    add `AddSingleRekeningDto`

---
+++ src/main/java/com/example/aeon/dtos/rekening/AddSingleRekeningDto.java
@@ -0,0 +1,14 @@
+package com.example.aeon.dtos.rekening;
+
+import lombok.Data;
+
+@Data
+public class AddSingleRekeningDto {
+  
+  private String nama;
+  
+  private String jenis;
+  
+  private String nomor;
+  
+}
